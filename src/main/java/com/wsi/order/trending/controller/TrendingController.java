ac9a7ebc13275e7d37e55a60539a2a75a17964e0
zalam <zalam@192.168.1.7>
2018-11-21 17:14:29 +0530

    itemvo converter into item service

--- src/main/java/com/wsi/order/trending/controller/TrendingController.java
+++ src/main/java/com/wsi/order/trending/controller/TrendingController.java
@@ -55,21 +55,10 @@ public class TrendingController {
     	Item item = itemsService.getItemById(itemid);
     	
     	if(item != null) {
-    		ItemVo itemVo = new ItemVo();
+    		ItemVo itemVo;
+    		ConvertEntityToVo convertEntityToVo = new ConvertEntityToVo();
+    		itemVo = convertEntityToVo.convertEntityToVo(item);
         	
-        	itemVo.setItemKey(item.getItemKey().trim());
-        	itemVo.setCostCurrency(item.getCostCurrency().trim());
-        	itemVo.setDescription(item.getDescription().trim());
-        	itemVo.setExtendedDesc(item.getExtendedDesc().trim());
-        	itemVo.setImageUrl(item.getImageUrl().trim());
-        	itemVo.setItemId(item.getItemId().trim());
-        	itemVo.setItemType(item.getItemType().trim());
-        	itemVo.setManufacturerItem(item.getManufacturerItem().trim());
-        	itemVo.setManufacturerItemDesc(item.getManufacturerItemDesc().trim());
-        	itemVo.setManufacturerName(item.getManufacturerName().trim());
-        	itemVo.setShortDesc(item.getShortDesc().trim());
-        	itemVo.setStatus(item.getStatus().trim());
-        	itemVo.setUnitCost(item.getUnitCost());
     		return ResponseEntity.ok().body(itemVo);
     	}
     	else {
